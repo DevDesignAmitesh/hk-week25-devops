7121ca970233b141941879eece2100b7b827ea2c
DevDesignAmitesh <amiteshsingh252@gmail.com>
2025-07-01 11:57:18 +0530

    added http backend

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import { prisma } from "@repo/db/db";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.post("/todo", async (req: Request, res: Response): Promise<any> => {
+  const { message, description } = req.body;
+
+  const todo = await prisma.todo.create({
+    data: {
+      message,
+      description,
+    },
+  });
+
+  return res.status(200).json({ message: "todo added", todo });
+});
+
+app.get("/todo", async (_, res: Response): Promise<any> => {
+  const todos = await prisma.todo.findMany();
+
+  return res.status(200).json({ message: "todos found", todos });
+});
+
+app.listen(PORT, () => {
+  console.log("server is runing on port", PORT);
+});
