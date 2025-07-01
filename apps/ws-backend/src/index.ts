412fd520bb675dfe9c21e78c1377416dd62643c8
DevDesignAmitesh <amiteshsingh252@gmail.com>
2025-07-01 12:14:07 +0530

    added websocket backend

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,24 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+const server = new WebSocketServer({ port: 8008 });
+
+server.on("connection", (ws: WebSocket) => {
+  ws.on("error", (e) => console.log(e));
+
+  ws.on("message", (data) => {
+    const parsedData = JSON.parse(data.toString());
+
+    const { message, description, type } = parsedData;
+    if (type === "todo") {
+      ws.send(
+        JSON.stringify({
+          message,
+          description,
+          type,
+        })
+      );
+    }
+  });
+
+  ws.on("close", (e) => console.log("connection is closed"));
+});
